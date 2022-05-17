41218a8238e7942f35040d57dac1972afa2602c8
pityulin <pityulin@gmail.com>
2022-05-18 00:12:26 +0300

    support for all OSs

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('./build/Release/win-event');
+const nonWinResponse = () => undefined
+const stub = {
+	start: nonWinResponse,
+	stop: nonWinResponse 
+}
+
+const os = require('os')
+
+module.exports = os.platform() === 'win32' ? require('./build/Release/win-event') : stub
